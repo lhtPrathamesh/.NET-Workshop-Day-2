1f370b41c3d0bd0e64258c88baffd61b7797e580
lhtPrathamesh <prathamesh@leewayhertz.com>
2023-06-30 13:52:25 +0530

    exception handling and asynchronous programming

---
+++ Exception-handling/AsyncAwait.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_handling
+{
+    public class AsyncAwait
+    {
+        public async Task<int> AyncAwaitSample ()
+        {
+            await Task.Delay (3000);
+            Console.WriteLine("AyncAwaitSample After delay of 3 sec");
+            return 1;
+        }
+
+        public async Task FetchDataAsync()
+        {
+            string apiUrl = "https://reqres.in/api/users?page=1";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string data = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(data);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Request failed with status code: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
---
+++ Exception-handling/Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+using Exception_handling;
+using System;
+
+class MyException
+{
+    public static async Task Main(string[] args)
+    {
+        BankAccount myAccount = new BankAccount(1000);
+        var exception = "";
+
+        try
+        {
+            myAccount.Withdraw(2000);
+        }
+        catch (CustomException ex)
+        {
+            exception = ex.Message;
+            Console.WriteLine($"{ex.Message}. Account Balance: {ex.accBalance}"); ;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        finally
+        {
+            if (exception.Length == 0) {
+                Console.WriteLine("Transaction Completed !");
+            } else
+            {
+                Console.WriteLine($"Transaction could not complete due to this error: {exception}");
+            }
+        }
+
+        // Async await implementation
+        AsyncAwait asyncAwait = new AsyncAwait();
+        var number = await asyncAwait.AyncAwaitSample();
+        Console.WriteLine($"Number After Delay: {number}");
+
+        // Fetch data asynchronously
+        await asyncAwait.FetchDataAsync();
+
+    }
+}
+
+public class BankAccount
+{
+    private decimal balance;
+
+    public BankAccount(decimal initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > balance)
+        {
+            throw new CustomException("Insufficient funds to make the transaction.", balance);
+        }
+
+        balance -= amount;
+        Console.WriteLine($"Successfully withdrawn {amount}. New balance: {balance}");
+    }
+}
+
+public class CustomException : Exception
+{
+    public decimal accBalance;
+
+    public CustomException(string message, decimal balance) : base(message)
+    {
+        accBalance = balance;
+    }
+}
